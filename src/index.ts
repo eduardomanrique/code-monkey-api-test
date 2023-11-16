8dd10a7f8c712b51b27937ee4c54c74ef318f7b2
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-16 10:52:49 +0000

    Update files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import bookRoutes from './routes/books';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 const port = 3000;
 
 app.use('/books', bookRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Code Monkey API');
