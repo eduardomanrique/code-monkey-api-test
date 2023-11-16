505eb52bf9f98a2701215ec082b89a3392992f26
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-16 17:48:20 +0000

    Update files

--- src/db.ts
+++ src/db.ts
@@ -7,3 +7,15 @@ export const openDb = async () => {
     driver: sqlite3.Database
   });
 };
+
+export const setupDatabase = async () => {
+  const db = await openDb();
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS books (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      author TEXT NOT NULL,
+      isbn TEXT NOT NULL
+    )
+  `);
+};
