2c228908e89dad1c45f12d39f10d02f9b705b7ef
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-19 15:28:05 +0000

    Update CORS configuration to allow PUT method

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const port = 3001;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
